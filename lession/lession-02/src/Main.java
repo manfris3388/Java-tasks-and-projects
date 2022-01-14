8a8ad25dd3d1230bf7e10a143538750d7031e9c4
manfris3388 <lmanfredas@gmail.com>
2022-01-14 12:08:55 +0200

    Update Main.java

--- lession/lession-02/src/Main.java
+++ lession/lession-02/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Labas pasauli!");
+        System.out.println("Labas pasauli! Kokia graži diena");
     }
 }
