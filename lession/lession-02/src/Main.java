a0794343c728fe1472d733ba54816dd10eb1c8a7
manfris3388 <lmanfredas@gmail.com>
2022-01-14 12:54:13 +0200

    [lession 02] testing name commit

--- lession/lession-02/src/Main.java
+++ lession/lession-02/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Labas pasauli! Kokia graži diena");
+        System.out.println("Labas, Manfredai");
     }
 }
