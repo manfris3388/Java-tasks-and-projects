be5dffb70ee9fc2d796ca0b335e87b809a76cc3f
manfris3388 <lmanfredas@gmail.com>
2022-01-25 09:57:36 +0200

    ND01 UZ01

---
+++ Homework/Homework01/src/main/java/lt/codeacademy/Main.java
@@ -0,0 +1,9 @@
+package lt.codeacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1.printTask1("Vienas");
+        String text = "Du";
+        Task1.printTask1(text);
+    }
+}
---
+++ Homework/Homework01/src/main/java/lt/codeacademy/Task1.java
@@ -0,0 +1,7 @@
+package lt.codeacademy;
+
+public class Task1 {
+    public static void printTask1(String text){
+        System.out.println(text);
+    }
+}
