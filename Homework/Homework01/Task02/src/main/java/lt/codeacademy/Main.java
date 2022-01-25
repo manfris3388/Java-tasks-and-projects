e486f3b09b579c4df2850a6550ce17310565ada3
manfris3388 <lmanfredas@gmail.com>
2022-01-25 11:14:54 +0200

    ND01 UZ02 Printing command line updated

---
+++ Homework/Homework01/Task02/src/main/java/lt/codeacademy/Main.java
@@ -0,0 +1,9 @@
+package lt.codeacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        for (String i : args) {
+            Task2.printTask2(i);
+        }
+    }
+}
---
+++ Homework/Homework01/Task02/src/main/java/lt/codeacademy/Task2.java
@@ -0,0 +1,7 @@
+package lt.codeacademy;
+
+public class Task2 {
+    public static void printTask2(String text){
+        System.out.println(text);
+    }
+}
