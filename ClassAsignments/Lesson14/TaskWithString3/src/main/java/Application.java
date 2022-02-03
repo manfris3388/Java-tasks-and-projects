f9573aca4970a9d05c0cb24e0f247f41743ee53c
manfris3388 <lmanfredas@gmail.com>
2022-02-03 21:20:42 +0200

    [Lesson014] adding to array improved

--- ClassAsignments/Lesson14/TaskWithString3/src/main/java/Application.java
+++ ClassAsignments/Lesson14/TaskWithString3/src/main/java/Application.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 
 public class Application {
     public static String[] texts = new String[0];
-
+    public static String[] newArr;
+    public static Boolean textAdded;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Įveskite tekstą");
-        String text = scanner.nextLine();
-        Boolean textAdded = addText(text);
-        System.out.println("Tekstas pridėtas: " + textAdded);
+        System.out.println("Įveskite tekstą, spauskit q pabaigti");
+        while (true) {
+            String text = scanner.nextLine();
+            if(text.equals("q")) break;
+            textAdded = addText(text);
+        }
+        System.out.println("Naujas masyvas" + Arrays.toString(texts));
+        scanner.close();
     }
 
     private static boolean addText(String text) {
-        String [] newArr = new String[texts.length+1];
-        if (text instanceof String && text != null) {
+        newArr = new String[texts.length+1];
+        if (text != null) {
             newArr[0] = text;
             for (int i = 0; i < texts.length; i++) {
                 newArr[i + 1] = texts[i];
             }
             System.out.println(Arrays.toString(newArr));
+            texts = newArr;
             return true;
         }
         else {
