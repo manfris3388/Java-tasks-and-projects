e3f481e670b19b35c7c4660145510bdbdf2326cc
manfris3388 <lmanfredas@gmail.com>
2022-05-16 11:08:26 +0300

    2. Attaching first template

---
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticchart/welcome/controller/HelloController.java
@@ -0,0 +1,14 @@
+package lt.codeacademy.anestheticchart.welcome.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("")
+    public String hello(){
+        return "hello";
+    }
+}
