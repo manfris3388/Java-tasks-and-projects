bbb73f91570b77efa8ff70a502ff68212d4ff042
manfris3388 <lmanfredas@gmail.com>
2022-05-17 13:39:08 +0300

    6. Finished mapping DTO to template through the model.

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/model/ChartDTO.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/model/ChartDTO.java
@@ -9,6 +9,25 @@ public class ChartDTO {
     private String name;
     private String surname;
     private String hospitalNumber;
-    private LocalDate dob;
-
+    private String dob;
+    private String operation;
+    private String pmh;
+    private String dh;
+    private String airwayAssessment;
+    private String anestheticAssessment;
+    private Integer sats;
+    private Integer rr;
+    private Integer hr;
+    private Integer sbp;
+    private Integer dbp;
+    private String otherVitals;
+    private Integer na;
+    private Double k;
+    private Double urea;
+    private Integer cr;
+    private String otherLabRes;
+    private String ecg;
+    private String otherIMG;
+    private String anestheticPlan;
+    private String anestheticWorkup;
 }
