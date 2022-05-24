6d03772933fee3e2502564082f9e47c694a08896
manfris3388 <lmanfredas@gmail.com>
2022-05-24 17:49:23 +0300

    24. Filling the table with some mock data

---
+++ Spring/Project/anestheticChart/src/main/resources/db/initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ANESTHETIC_CHARTS(id, uuid, name, surname, hospital_number, dob, operation)
+VALUES
+(1, '47ab05ac-db6e-11ec-9d64-0242ac120002', 'Jonas', 'Jonaitis', 'D123456', '1972-05-27', 'Appendectomy'),
+(2, '47ab0796-db6e-11ec-9d64-0242ac120002', 'Petras', 'Petraitis', 'D654321', '1984-04-11', 'Tonsillectomy');
