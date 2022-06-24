87d53ea9247c20cf41bcef677044346166bf3ea3
manfris3388 <lmanfredas@gmail.com>
2022-06-24 14:00:28 +0300

    105. Approaching tests

--- Spring/Project/anestheticChart/src/test/java/lt/codeacademy/anestheticChart/AnestheticChartApplicationTestsDTO.java
+++ Spring/Project/anestheticChart/src/test/java/lt/codeacademy/anestheticChart/AnestheticChartApplicationTestsDTO.java
@@ -3,10 +3,10 @@ package lt.codeacademy.anestheticChart;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AnestheticChartApplicationTestsDTO {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
