7e5fed57ebfc95573fca6e5f32aecfb5e1e409c7
manfris3388 <lmanfredas@gmail.com>
2022-06-29 10:34:53 +0300

    2. Created fragment in a component

--- React/experimenting/untitled/src/components/MyCoolComponent.js
+++ React/experimenting/untitled/src/components/MyCoolComponent.js
@@ -2,11 +2,18 @@ import React from "react";
 
 // export default (props) => <h1> Hello, {props.name}!</h1>
 
-export default class MyCoolComponent extends React.Component{
+export default class MyCoolComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {name: 'World'}
     }
 
-    render() {return <h1> Hello, {this.props.name}</h1>
+    render() {
+        return(
+            <>
+                <input type={'Text'} name={'Manfris'}/>
+                <h1> Hello, {this.state.name}</h1>
+            </>
+        )
     }
 }
