137d08bdf9fcf6f45f36e72a3563fb27a94a0555
manfris3388 <lmanfredas@gmail.com>
2022-07-10 10:15:45 +0300

    3. Swagger api documentation information

---
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package lt.codeacademy.anestheticChart.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(getInfo());
+    }
+
+    private static ApiInfo getInfo() {
+        return new ApiInfo(
+                "Anesthetic Chart RestFull Api Documentation",
+                "This is simple documentation using Swagger and SpringFox",
+                "0.0.1",
+                "Anesthetic Chart term URL",
+                new Contact("Vardenis Pavardenis", "www.codeacademy.lt", "a@codeacademy.lt"),
+                "Anesthetic Chart licence",
+                "Licence URL",
+                Collections.emptyList()
+        );
+    }
+}
