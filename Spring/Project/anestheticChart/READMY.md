f3ca12b08085ffe4ab12a7314cea8d52c39401de
manfris3388 <lmanfredas@gmail.com>
2022-07-11 13:09:30 +0300

    10. Created directory through Java to save files

--- Spring/Project/anestheticChart/READMY.md
+++ Spring/Project/anestheticChart/READMY.md
@@ -1,2 +1,10 @@
 ### Access Swagger API
 * http://localhost:8080/swagger-ui/index.html
+
+### working with profile postgres
+### h2 profile is inactive at the moment
+
+### to run database mock schemas  you can find them in resources/db
+
+### at the moment if you want to access api work with profile security-off
+### otherwise if you want to check mvc security don't use it
--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/FileService.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/FileService.java
@@ -1,7 +1,26 @@
 package lt.codeacademy.anestheticChart.api.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
+@Slf4j
 public class FileService {
+    private final Path fileLocation = Paths.get("./files").toAbsolutePath().normalize();
+    public void saveFile(MultipartFile file) {
+        try {
+            if (!Files.exists(fileLocation)) {
+                Files.createDirectory(fileLocation);
+            }
+        } catch (IOException e) {
+            log.error("Cannot create directory", e);
+            e.printStackTrace();
+        }
+    }
 }
