083a22d6b060e0a83d09f925ab7390027b53ef6b
manfris3388 <lmanfredas@gmail.com>
2022-07-11 15:37:12 +0300

    15. Initial work with file download

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/FileController.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/FileController.java
@@ -2,6 +2,8 @@ package lt.codeacademy.anestheticChart.api.controller;
 
 import lombok.AllArgsConstructor;
 import lt.codeacademy.anestheticChart.api.service.FileService;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,11 @@ public class FileController {
     @PostMapping("api/file/upload")
     public void saveFile(@RequestParam MultipartFile file) {
         fileService.saveFile(file);
+    }
+
+
+    @GetMapping("/api/file/download")
+    public Resource getFileByFileName(@RequestParam String fileName) {
+        return fileService.getFile(fileName);
     }
 }
--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/FileService.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/FileService.java
@@ -1,10 +1,13 @@
 package lt.codeacademy.anestheticChart.api.service;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,33 +17,46 @@ import java.time.LocalDateTime;
 @Service
 @Slf4j
 public class FileService {
-    private final Path fileLocation = Paths.get("./files").toAbsolutePath().normalize();
-    public void saveFile(MultipartFile file) {
-        createDirectory();
+  private final Path fileLocation = Paths.get("./files").toAbsolutePath().normalize();
 
-        try {
-            Path filePathWithFileName = fileLocation.resolve(getUniqFileName(file));
-            Files.copy(file.getInputStream(), filePathWithFileName , StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            log.error("Cannot create file", e);
-            e.printStackTrace();
-        }
-    }
+  public void saveFile(MultipartFile file) {
+    createDirectory();
 
-    private String getUniqFileName(MultipartFile file) {
-        String fileName = file.getOriginalFilename();
-        int nanoDate = LocalDateTime.now().getNano();
-        return String.format("%s_%s", nanoDate, fileName);
+    try {
+      Path filePathWithFileName = fileLocation.resolve(getUniqFileName(file));
+      Files.copy(file.getInputStream(), filePathWithFileName, StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException e) {
+      log.error("Cannot create file", e);
+      e.printStackTrace();
     }
+  }
 
-    private void createDirectory() {
-        try {
-            if (!Files.exists(fileLocation)) {
-                Files.createDirectory(fileLocation);
-            }
-        } catch (IOException e) {
-            log.error("Cannot create directory", e);
-            e.printStackTrace();
-        }
+  private String getUniqFileName(MultipartFile file) {
+    String fileName = file.getOriginalFilename();
+    int nanoDate = LocalDateTime.now().getNano();
+    return String.format("%s_%s", nanoDate, fileName);
+  }
+
+  private void createDirectory() {
+    try {
+      if (!Files.exists(fileLocation)) {
+        Files.createDirectory(fileLocation);
+      }
+    } catch (IOException e) {
+      log.error("Cannot create directory", e);
+      e.printStackTrace();
     }
+  }
+
+  public Resource getFile(String fileName) {
+    try {
+      InputStream inputStream = Files.newInputStream(fileLocation.resolve(fileName));
+
+      return new InputStreamResource(inputStream);
+    } catch (IOException e) {
+      log.error("Cannot get/create file by file name", e);
+      e.printStackTrace();
+    }
+    return null;
+  }
 }
