b087ab07f8857b02ed416007a522385646c74e54
manfris3388 <lmanfredas@gmail.com>
2022-07-11 16:06:52 +0300

    15. Get file by hard coded media type

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/FileController.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/FileController.java
@@ -3,6 +3,8 @@ package lt.codeacademy.anestheticChart.api.controller;
 import lombok.AllArgsConstructor;
 import lt.codeacademy.anestheticChart.api.service.FileService;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +24,9 @@ public class FileController {
 
 
     @GetMapping("/api/file/download")
-    public Resource getFileByFileName(@RequestParam String fileName) {
-        return fileService.getFile(fileName);
+    public ResponseEntity<Resource> getFileByFileName(@RequestParam String fileName) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(fileService.getFile(fileName));
     }
 }
