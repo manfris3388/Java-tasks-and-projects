44071ddf9b6ac2da2909dbcb5d7b26104da9322d
manfris3388 <lmanfredas@gmail.com>
2022-07-12 08:47:33 +0300

    22. Moved api annotations to interface

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/ChartApiController.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/ChartApiController.java
@@ -1,9 +1,5 @@
 package lt.codeacademy.anestheticChart.api.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lt.codeacademy.anestheticChart.api.dto.ChartsResponse;
 import lt.codeacademy.anestheticChart.mvc.chart.dto.FullChartDTO;
@@ -11,79 +7,44 @@ import lt.codeacademy.anestheticChart.mvc.chart.service.ChartService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ChartApiController.CHARTS_ROOT_PATH)
-@Api(tags = "Chart Controller")
-public class ChartApiController {
-    public static final String CHARTS_ROOT_PATH = "/api/charts";
-    private static final String UUID_PATH = "/api/{uuid}";
+public class ChartApiController implements ChartApiSpecs {
 
-    private final ChartService chartService;
+  private final ChartService chartService;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    @ApiOperation(
-            value = "Get all charts",
-            notes = "Get all charts from db, and any other information could be here")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Kai sekmingai grazina anestezijos lapus"),
-            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant anestezijos lapus"),
-            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti anestezijos lapu sarasa")
-    })
-    public ChartsResponse getCharts() {
-        return ChartsResponse.builder()
-                .charts(chartService.getCharts())
-                .build();
+  public ChartsResponse getCharts() {
+    return ChartsResponse.builder().charts(chartService.getCharts()).build();
+  }
+
+  public ChartsResponse getCharts(@PathVariable("uuid") UUID uuid) {
+    return ChartsResponse.builder().charts(List.of(chartService.getFullChartByUUID(uuid))).build();
+  }
+
+  public Page<FullChartDTO> getChartsPaginated(
+      @RequestParam("page") int page, @RequestParam("size") int size) {
+    return chartService.getChartsPaginated(PageRequest.of(page, size));
+  }
+
+  public void deleteChart(UUID chartUUID) {
+    chartService.deleteChart(chartUUID);
+  }
+
+  public ResponseEntity<Void> createChart(FullChartDTO fullChartDTO) {
+
+    chartService.addChart(fullChartDTO);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  public ResponseEntity<Void> updateChart(FullChartDTO fullChartDTO) {
+    if (chartService.updateChart(fullChartDTO)) {
+      return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
-    @GetMapping(
-            path = UUID_PATH,
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    @ApiOperation(value = "Get one chart by id")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Kai sekmingai grazina anestezijos lapa"),
-            @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant anestezijos lapa"),
-            @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti anestezijos lapa")
-    })
-    public ChartsResponse getCharts(@PathVariable("uuid") UUID uuid) {
-        return ChartsResponse.builder()
-                .charts(List.of(chartService.getFullChartByUUID(uuid)))
-                .build();
-    }
-
-    @GetMapping("/page")
-    public Page<FullChartDTO> getChartsPaginated(@RequestParam("page") int page, @RequestParam("size") int size) {
-        return chartService.getChartsPaginated(PageRequest.of(page, size));
-    }
-
-    @DeleteMapping(path = UUID_PATH)
-    @ApiOperation(value = "Delete chart", httpMethod = "DELETE")
-    public void deleteChart(@PathVariable("uuid") UUID chartUUID) {
-        chartService.deleteChart(chartUUID);
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createChart(@Valid @RequestBody FullChartDTO fullChartDTO) {
-
-        chartService.addChart(fullChartDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @PutMapping
-    @ApiOperation(value = "Update chart", httpMethod = "PUT")
-    public ResponseEntity<Void> updateChart(@Valid @RequestBody FullChartDTO fullChartDTO) {
-        if (chartService.updateChart(fullChartDTO)) {
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }
-
-        return ResponseEntity.notFound().build();
-    }
+    return ResponseEntity.notFound().build();
+  }
 }
---
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/ChartApiSpecs.java
@@ -0,0 +1,62 @@
+package lt.codeacademy.anestheticChart.api.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lt.codeacademy.anestheticChart.api.dto.ChartsResponse;
+import lt.codeacademy.anestheticChart.mvc.chart.dto.FullChartDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RequestMapping(ChartApiController.CHARTS_ROOT_PATH)
+@Api(tags = "Chart Controller")
+public interface ChartApiSpecs {
+  String CHARTS_ROOT_PATH = "/api/charts";
+  String UUID_PATH = "/api/{uuid}";
+
+  @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @ApiOperation(
+      value = "Get all charts",
+      notes = "Get all charts from db, and any other information could be here")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Kai sekmingai grazina anestezijos lapus"),
+        @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant anestezijos lapus"),
+        @ApiResponse(
+            code = 403,
+            message = "Neturite reikalingu teisiu gauti anestezijos lapu sarasa")
+      })
+  ChartsResponse getCharts();
+
+  @GetMapping(
+      path = UUID_PATH,
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @ApiOperation(value = "Get one chart by id")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Kai sekmingai grazina anestezijos lapa"),
+        @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant anestezijos lapa"),
+        @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti anestezijos lapa")
+      })
+  ChartsResponse getCharts(@PathVariable("uuid") UUID uuid);
+
+  @GetMapping("/page")
+  Page<FullChartDTO> getChartsPaginated(
+      @RequestParam("page") int page, @RequestParam("size") int size);
+
+  @DeleteMapping(path = UUID_PATH)
+  @ApiOperation(value = "Delete chart", httpMethod = "DELETE")
+  void deleteChart(@PathVariable("uuid") UUID chartUUID);
+
+  @PostMapping
+  ResponseEntity<Void> createChart(@Valid @RequestBody FullChartDTO fullChartDTO);
+
+  @PutMapping
+  @ApiOperation(value = "Update chart", httpMethod = "PUT")
+  ResponseEntity<Void> updateChart(@Valid @RequestBody FullChartDTO fullChartDTO);
+}
