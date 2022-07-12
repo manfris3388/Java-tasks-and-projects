c67d46534c7b043cd4001c4c6953ad3dbca165c2
manfris3388 <lmanfredas@gmail.com>
2022-07-12 08:23:21 +0300

    21. Implemented update product in api

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/ChartApiController.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/ChartApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lt.codeacademy.anestheticChart.api.dto.ChartsResponse;
 import lt.codeacademy.anestheticChart.mvc.chart.dto.FullChartDTO;
 import lt.codeacademy.anestheticChart.mvc.chart.service.ChartService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,10 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(ChartApiController.PRODUCTS_ROOT_PATH)
+@RequestMapping(ChartApiController.CHARTS_ROOT_PATH)
 @Api(tags = "Chart Controller")
 public class ChartApiController {
-    public static final String PRODUCTS_ROOT_PATH = "/api/charts";
+    public static final String CHARTS_ROOT_PATH = "/api/charts";
     private static final String UUID_PATH = "/api/{uuid}";
 
     private final ChartService chartService;
@@ -51,14 +53,19 @@ public class ChartApiController {
             @ApiResponse(code = 401, message = "Reikalauja prisijungimo gaunant anestezijos lapa"),
             @ApiResponse(code = 403, message = "Neturite reikalingu teisiu gauti anestezijos lapa")
     })
-
     public ChartsResponse getCharts(@PathVariable("uuid") UUID uuid) {
         return ChartsResponse.builder()
                 .charts(List.of(chartService.getFullChartByUUID(uuid)))
                 .build();
     }
 
+    @GetMapping("/page")
+    public Page<FullChartDTO> getChartsPaginated(@RequestParam("page") int page, @RequestParam("size") int size) {
+        return chartService.getChartsPaginated(PageRequest.of(page, size));
+    }
+
     @DeleteMapping(path = UUID_PATH)
+    @ApiOperation(value = "Delete chart", httpMethod = "DELETE")
     public void deleteChart(@PathVariable("uuid") UUID chartUUID) {
         chartService.deleteChart(chartUUID);
     }
@@ -71,7 +78,8 @@ public class ChartApiController {
     }
 
     @PutMapping
-    public ResponseEntity<Void> updateProduct(@Valid @RequestBody FullChartDTO fullChartDTO) {
+    @ApiOperation(value = "Update chart", httpMethod = "PUT")
+    public ResponseEntity<Void> updateChart(@Valid @RequestBody FullChartDTO fullChartDTO) {
         if (chartService.updateChart(fullChartDTO)) {
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }
