85702d203a10b2d272f16196b8c9c38faec9a535
manfris3388 <lmanfredas@gmail.com>
2022-07-15 08:49:02 +0300

    28. Forgot to add JwtAuthenticationFilter

---
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package lt.codeacademy.anestheticChart.api.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lt.codeacademy.anestheticChart.api.dto.LoginRequest;
+import lt.codeacademy.anestheticChart.jpa.entity.user_mvc.User;
+import lt.codeacademy.anestheticChart.mvc.user.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+    private final UserService userService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, ObjectMapper objectMapper, UserService userService) {
+        super(authenticationManager);
+        this.objectMapper = objectMapper;
+        this.userService = userService;
+    }
+
+    //HttpServletRequest passes request information to servlet()
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            //sumapina loginDto
+            LoginRequest loginDto = objectMapper.readValue(request.getReader(), LoginRequest.class);
+
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+                    loginDto.getUsername(), loginDto.getPassword());
+            // Allow subclasses to set the "details" property
+            setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        } catch (IOException e) {
+            throw new BadCredentialsException("Unable to parse payload credentials");
+        }
+    }
+
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        //
+    }
+
+}
