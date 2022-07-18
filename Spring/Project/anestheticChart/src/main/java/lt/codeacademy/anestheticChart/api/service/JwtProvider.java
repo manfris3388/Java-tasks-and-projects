9b33e5656b7fc7c6cb8ec75ae08b75cb39e15407
manfris3388 <lmanfredas@gmail.com>
2022-07-18 21:30:42 +0300

    Revert "34. Fixed security on both mvc and rest application."
    This reverts commit 1a062ae1622ea976ca11554ed22a23db678e869a.

--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/JwtProvider.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/service/JwtProvider.java
@@ -1,39 +1,11 @@
 package lt.codeacademy.anestheticChart.api.service;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import lt.codeacademy.anestheticChart.mvc.user.dto.UserRoleDto;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
-
-  private final static Date NOW = new Date();
-
-  @Value("#{${security.jwt.validity-time} * 60 * 1000}")
-  private long tokenValidityInMillis;
-
-  @Value("${security.jwt.secret-key}")
-  private byte[] secretKey;
-
-  public String getToken(UserRoleDto principal) {
-    return Jwts.builder()
-            .setHeaderParam("typ", "JWT")
-            .setIssuer("anestheticChart-api")
-            .setAudience("anestheticChart-ui")
-            .setSubject(principal.getUsername())
-            .setIssuedAt(NOW)
-            .setExpiration(new Date(NOW.getTime() + tokenValidityInMillis))
-            .claim("roles", principal.getAuthorities().stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.toSet()))
-            .signWith(Keys.hmacShaKeyFor(secretKey), SignatureAlgorithm.HS512)
-            .compact();
-  }
+    public String getToken(UserRoleDto principal) {
+        return "My token: " + principal.getFullName();
+    }
 }
