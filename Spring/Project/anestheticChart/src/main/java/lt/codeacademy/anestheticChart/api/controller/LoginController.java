6911543a26f55a5dc8baa293d70a3c49ef4365ee
manfris3388 <lmanfredas@gmail.com>
2022-07-20 17:42:00 +0300

    43. Returning response after logging in

---
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/controller/LoginController.java
@@ -0,0 +1,18 @@
+package lt.codeacademy.anestheticChart.api.controller;
+
+import lt.codeacademy.anestheticChart.api.dto.LoginResponse;
+import lt.codeacademy.anestheticChart.mvc.user.dto.UserRoleDto;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @PostMapping
+    public LoginResponse login(@AuthenticationPrincipal UserRoleDto userRoleDto) {
+        return new LoginResponse(userRoleDto);
+    }
+}
--- Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/filter/JwtAuthenticationFilter.java
+++ Spring/Project/anestheticChart/src/main/java/lt/codeacademy/anestheticChart/api/filter/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -52,7 +53,9 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        SecurityContextHolder.getContext().setAuthentication(authResult);
         response.addHeader(HttpHeaders.AUTHORIZATION, jwtProvider.getToken((UserRoleDto) authResult.getPrincipal()));
+        chain.doFilter(request, response);
     }
 
 }
