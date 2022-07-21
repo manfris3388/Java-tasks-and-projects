04382ef6499cb085c9068bbfec84f43791009c73
manfris3388 <lmanfredas@gmail.com>
2022-07-21 20:08:37 +0300

    6. Created content and footer containers

--- React/ui/src/containers/App.js
+++ React/ui/src/containers/App.js
@@ -1,8 +1,12 @@
 import HeaderContainer from './header/HeaderContainer';
+import ContentContainer from "./content/ContentContainer";
+import FooterContainer from "./footer/FooterContainer";
 
 function App() {
     return <div>
         <HeaderContainer />
+        <ContentContainer />
+        <FooterContainer />
     </div>;
 }
 
