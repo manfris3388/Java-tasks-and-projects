fa54a48921d2954459fe3c30a8e7aa8ea8649361
manfris3388 <lmanfredas@gmail.com>
2022-07-22 09:22:10 +0300

    10. Started working with Router

--- React/ui/src/index.js
+++ React/ui/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './containers/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./containers/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
     <React.StrictMode>
         <App />
