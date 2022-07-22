14ef84fee2fec24285860dedbac5c6d39d43a048
manfris3388 <lmanfredas@gmail.com>
2022-07-22 09:22:10 +0300

    10. Started working with Router

--- React/ui/src/containers/App.js
+++ React/ui/src/containers/App.js
@@ -1,14 +1,21 @@
 import HeaderContainer from './header/HeaderContainer';
-import ContentContainer from "./content/ContentContainer";
 import FooterContainer from "./footer/FooterContainer";
 import './App.css';
+import {BrowserRouter,Routes, Route} from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
-    return <div className='mainApp'>
-        <HeaderContainer />
-        <ContentContainer />
-        <FooterContainer />
-    </div>;
+    return (
+        <BrowserRouter>
+            <div className='mainApp'>
+                <HeaderContainer />
+                <Routes>
+                    <Route path='/' element={ <HomePage /> } />
+                </Routes>
+                <FooterContainer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
