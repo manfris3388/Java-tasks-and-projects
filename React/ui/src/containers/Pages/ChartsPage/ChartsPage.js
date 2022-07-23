f0fbd9bb483ea5cee58ec5cce87e2aca9ba4ed59
manfris3388 <lmanfredas@gmail.com>
2022-07-23 18:55:32 +0300

    26. Displaying patient charts using bootstrap.

--- React/ui/src/containers/Pages/ChartsPage/ChartsPage.js
+++ React/ui/src/containers/Pages/ChartsPage/ChartsPage.js
@@ -1,19 +1,44 @@
 
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import {getChartsApi} from "../../../api/ApiEndpoints";
+import { Card, Col, Container, Row } from 'react-bootstrap';
 
 const ChartsPage = () => {
 
+    const [chartFields, setChartFields] = useState([])
+
     useEffect(() => {
         getChartsApi()
-            .then((response) => console.log('response', response))
+            .then(({data}) => {
+                console.log('response', data)
+                setChartFields(data.charts)
+            })
             .catch((error) => console.log('error', error))
     })
 
     return (
-        <>
-            <h1>Charts list</h1>
-        </>
+        <Container fluid>
+            <h1 className={"text-center"}>Charts list</h1>
+            <Row xs={1} md={2} className="g-4">
+                {chartFields.map(chart => (
+                    <Col key={chart.chartId}>
+                        <Card>
+                            <Card.Header>
+                                <div className="d-flex justify-content-lg-around">
+                                    <div>{chart.name}</div>
+                                    <div>{chart.surname}</div>
+                                </div>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Title>{chart.hospitalNumber}</Card.Title>
+                                <Card.Text>{chart.dob}</Card.Text>
+                                <Card.Text>{chart.operation}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
